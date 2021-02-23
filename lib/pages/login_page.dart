3e7d2e527f739fb4e00d83a40253e6c3328b9ee8
Joaquin Amenta <joaquin.amenta@gmail.com>
2021-02-23 16:10:39 -0300

    Fin seccion 9 - Login y registro

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,15 @@
-import 'package:chat/widgets/boton_azul.dart';
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
 
 import 'package:chat/widgets/logo.dart';
 import 'package:chat/widgets/labels.dart';
 import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/boton_azul.dart';
+import 'package:chat/services/auth_service.dart';
+import 'package:chat/helpers/mostrar_alerta.dart';
+
+
 
 class LoginPage extends StatelessWidget {
 
@@ -54,6 +59,11 @@ class __FormState extends State<_Form> {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>( context );
+
+
+
     return Container(
       margin: EdgeInsets.only(top: 40),
       padding: EdgeInsets.symmetric(horizontal: 50),
@@ -76,9 +86,21 @@ class __FormState extends State<_Form> {
 
           BotonAzul(
             text: 'Ingrese',
-            onPressed: (){
-              print(emailCtrl.text);
-              print(passCtrl.text);
+            onPressed: authService.autenticando ? null : () async {
+
+              FocusScope.of(context).unfocus();
+
+              final loginOk = await authService.login(emailCtrl.text.trim(), passCtrl.text.trim()); 
+
+              if(loginOk){
+                // TODO: conectar al socket server
+                Navigator.pushReplacementNamed(context, 'usuarios');
+              }else{
+                // Mostrar alerta
+                mostrarAlerta(context, 'Login Incorrecto', 'Revisar las credenciales');
+
+              }
+
             },
           ),
 
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,10 +1,13 @@
-import 'package:chat/widgets/boton_azul.dart';
+import 'package:chat/helpers/mostrar_alerta.dart';
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
 
 import 'package:chat/widgets/logo.dart';
 import 'package:chat/widgets/labels.dart';
 import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/boton_azul.dart';
+import 'package:chat/services/auth_service.dart';
 
 class RegisterPage extends StatelessWidget {
 
@@ -55,6 +58,9 @@ class __FormState extends State<_Form> {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>(context);
+
     return Container(
       margin: EdgeInsets.only(top: 40),
       padding: EdgeInsets.symmetric(horizontal: 50),
@@ -83,10 +89,20 @@ class __FormState extends State<_Form> {
           ),
 
           BotonAzul(
-            text: 'Ingrese',
-            onPressed: (){
+            text: 'Crear Cuenta',
+            onPressed: authService.autenticando ? null : () async {
+              print(nameCtrl.text);
               print(emailCtrl.text);
               print(passCtrl.text);
+              final registroOk = await authService.register(nameCtrl.text.trim(), emailCtrl.text.trim(), passCtrl.text.trim());
+
+              if( registroOk == true ){
+                // TODO: Conectar 
+                Navigator.pushReplacementNamed(context, 'usuarios');
+              }else{
+                mostrarAlerta(context, 'Registro incorrecto', registroOk );
+              }
+
             },
           ),
 
